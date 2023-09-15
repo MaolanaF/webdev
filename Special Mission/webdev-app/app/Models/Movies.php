60ce2f1200f4b34b9bb1bb7a6d0a4224a3482c6c
MaolanaF <maolana.firmansyah.tif421@polban.ac.id>
2023-09-15 17:00:16 +0700

    Create Database

---
+++ Special Mission/webdev-app/app/Models/Movies.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movies extends Model
+{
+    protected $table = 'movies';
+    protected $primarykey = 'id';
+    protected $fillable = ['title', 'description', 'genre', 'sutradara'];
+    
+    use HasFactory;
+}
